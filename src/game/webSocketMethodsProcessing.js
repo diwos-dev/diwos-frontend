d078b6bd8af6f8dd7b108b09a45b886ae647d61f
Vadim <vademcheg@gmail.com>
2022-04-30 13:03:02 +0400

    webSocketMethodsProcessing ping new Date(wsResponse.date) fix

--- src/game/webSocketMethodsProcessing.js
+++ src/game/webSocketMethodsProcessing.js
@@ -2,9 +2,9 @@
 
 function ping(ws, wsResponse) {
 
-    console.log(new Date() - wsResponse.date);
+    console.log(new Date() - new Date(wsResponse.date));
     
-    return new Date() - wsResponse.date;
+    return new Date() - new Date(wsResponse.date);
 
 }
 
